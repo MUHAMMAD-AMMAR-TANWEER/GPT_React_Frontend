d6cdf62350bc5896ea1c468daf9866ec515df000
Muhammad Ammar Tanweer <49334070+MUHAMMAD-AMMAR-TANWEER@users.noreply.github.com>
2021-11-23 17:15:35 +0500

    set components structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react' //write rafce and code snippit will be written
+
+import { Article, Brand, CTA } from './components';
+
+const App = () => {
+    return (
+        <div>
+            <h1>GPT-3</h1>
+
+        </div>
+    )
+}
+
+export default App
